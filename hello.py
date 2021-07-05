9467ca6018c1e0a7d360d413228b44ce561d8d04
kongsj <ufo0623@naver.com>
2021-07-05 14:58:49 +0900

    issue #1

--- hello.py
+++ hello.py
@@ -1,4 +1,15 @@
 #! /usr/bin/python
 
-print("Hello Bioinformatics")
+import sys
+
+print(sys.argv) #sys.argv는 리스트list
+# sys.exit() #여기까지만 실행
+
+
+name = sys.argv[1]     
+ #sys.argv는 argv를 받아와서 sys에 넣는다? 
+name2 = sys.argv[2]
+
+print(f"Hello {name}")
+print(f"Hello {name2}")
 
