64d2e5e03a3e12d09d8c1e50f7c82f40d1710a56
kongsj <ufo0623@naver.com>
2021-07-05 15:19:34 +0900

    #1 get name

--- hello.py
+++ hello.py
@@ -6,8 +6,7 @@ print(sys.argv) #sys.argv는 리스트list
 # sys.exit() #여기까지만 실행
 
 
-name = sys.argv[1]     
- #sys.argv는 argv를 받아와서 sys에 넣는다? 
+name = sys.argv[1]      
 name2 = sys.argv[2]
 
 print(f"Hello {name}")
